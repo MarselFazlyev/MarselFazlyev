a963f137259d8ee560126a59b8fa8147941d3cb9
Marsel <fazlyev@mail.ru>
2022-08-04 00:03:11 +0300

    Add files via upload

---
+++ java-homeworks/queue/4.2.1/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Integer integer = new Integer();
+
+        Arr
+    }
+}
